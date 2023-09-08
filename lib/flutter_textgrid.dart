76875af1aa417d72d5e3d2486c97670adb030fd3
Ciro Carandente <ciro.carandente@protonmail.com>
2023-09-08 21:02:58 +0200

    feat(master): added serializer to export

--- lib/flutter_textgrid.dart
+++ lib/flutter_textgrid.dart
@@ -7,4 +7,6 @@ export 'src/tier/interval_tier.dart';
 export 'src/annotation/annotation.dart';
 export 'src/annotation/interval_annotation.dart';
 export 'src/annotation/point_annotation.dart';
+export 'src/io/text_grid_serializer.dart';
+export 'src/io/text_grid_serializer_interface.dart';
 export 'src/utils/utils.dart';
